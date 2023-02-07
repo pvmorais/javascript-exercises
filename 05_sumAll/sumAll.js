09e487cc4f2282d63b331f893fdfdf82e7608b1c
Vinicius de Morais <pvmoraiss@gmail.com>
2023-02-06 19:18:37 -0600

    exercise 05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+      return 'ERROR';
+    }
+  
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      sum += i;
+    }
+  
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
